c74ce915b1b4bd501aba9ee94d719536387f86eb
Alex Makhotkin <54326502+cucumberMahach@users.noreply.github.com>
2023-05-02 03:32:57 +0300

    Initial commit

---
+++ screendemo/HttpServer.cs
@@ -0,0 +1,165 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace screendemo
+{
+    class Params
+    {
+        [JsonPropertyName("format")] public string format { get; set; }
+        [JsonPropertyName ("quality")] public int quality { get; set; }
+    }
+    class HttpServer
+    {
+        private HttpListener listener;
+        private int requestCount = 0;
+        private byte[] image;
+        private object locker = new object();
+        private Params param;
+
+        public EventHandler<Params> ParamsChanged;
+
+        public HttpServer()
+        {
+            image = File.ReadAllBytes("plug.jpg");
+            param = new Params();
+            param.format = "png";
+            param.quality = 100;
+        }
+
+        public Params GetParams()
+        {
+            return param;
+        }
+
+        public void Start(string url)
+        {
+            listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Listening for connections on {0}", url);
+
+            Task listenTask = HandleIncomingConnections();
+            //listenTask.GetAwaiter().GetResult();
+
+            //listener.Close();
+        }
+
+        public void Stop()
+        {
+            listener.Stop();
+        }
+
+        public void PutImage(byte[] data)
+        {
+            lock (locker)
+            {
+                image = data;
+            }
+        }
+
+        public byte[] GetImage()
+        {
+            lock(locker)
+            {
+                return image.ToArray();
+            }
+        }
+
+        public async Task HandleIncomingConnections()
+        {
+            bool runServer = true;
+
+            // While a user hasn't visited the `shutdown` url, keep on handling requests
+            while (runServer)
+            {
+                // Will wait here until we hear from a connection
+                HttpListenerContext ctx = await listener.GetContextAsync();
+
+                // Peel out the requests and response objects
+                HttpListenerRequest req = ctx.Request;
+                HttpListenerResponse resp = ctx.Response;
+
+                // Print out some info about the request
+                Console.WriteLine("Request #: {0}", ++requestCount);
+                Console.WriteLine(req.Url.ToString());
+                Console.WriteLine(req.HttpMethod);
+                Console.WriteLine(req.UserHostName);
+                Console.WriteLine(req.UserAgent);
+                Console.WriteLine();
+
+                // If `shutdown` url requested w/ POST, then shutdown the server after serving the page
+                if (req.HttpMethod == "POST" && req.Url.AbsolutePath == "/api/setParams")
+                {
+                    var stream = req.InputStream;
+                    string[] str;
+                    using (var reader = new StreamReader(stream))
+                    {
+                        str = reader.ReadToEnd().Split('&');
+                    }
+
+                    Params p = new Params();
+
+                    foreach (string pair in str)
+                    {
+                        string[] splitted = pair.Split('=');
+                        string key = splitted[0];
+                        string value = splitted[1];
+                        switch (key)
+                        {
+                            case "format":
+                                p.format = value;
+                                break;
+                            case "quality":
+                                p.quality = int.Parse(value);
+                                break;
+                        }
+                    }
+
+                    param = p;
+
+                    ParamsChanged?.Invoke(this, p);
+
+                    resp.Redirect("/");
+                    resp.Close();
+                }
+                else if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/img")
+                {
+                    // Write the response info
+                    byte[] data = GetImage();
+                    resp.ContentType = "image/png";
+                    resp.ContentEncoding = null;
+                    resp.ContentLength64 = data.LongLength;
+
+                    // Write out to the response stream (asynchronously), then close it
+                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                    resp.Close();
+                }
+                else if (req.HttpMethod == "GET" && req.Url.AbsolutePath == "/api/params")
+                {
+                    string json = JsonSerializer.Serialize<Params>(GetParams());
+                    byte[] data = Encoding.UTF8.GetBytes(json);
+                    resp.ContentType = "application/json";
+                    resp.ContentEncoding = Encoding.UTF8;
+                    resp.ContentLength64 = data.LongLength;
+                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                    resp.Close();
+                }
+                else
+                {
+                    // Write the response info
+                    string pageData = File.ReadAllText("server.html");
+                    byte[] data = Encoding.UTF8.GetBytes(pageData);
+                    resp.ContentType = "text/html";
+                    resp.ContentEncoding = Encoding.UTF8;
+                    resp.ContentLength64 = data.LongLength;
+
+                    // Write out to the response stream (asynchronously), then close it
+                    await resp.OutputStream.WriteAsync(data, 0, data.Length);
+                    resp.Close();
+                }
+            }
+        }
+    }
+}
---
+++ screendemo/Program.cs
@@ -0,0 +1,137 @@
+﻿using screendemo;
+using System;
+using System.Drawing.Imaging;
+using System.Net;
+using WebSocketSharp.Server;
+using WebSocketSharp;
+using System.Diagnostics;
+
+namespace screendemo
+{
+    public class Laputa : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Use tx, txlocal or rx in args");
+                return;
+            }
+
+            if (args[0] == "tx")
+            {
+                Console.WriteLine("tx");
+
+                var wssv = new WebSocketServer("ws://127.0.0.1:8001/");
+                wssv.AddWebSocketService<Laputa>("/stream");
+                wssv.Start();
+
+                ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+                EncoderParameters codecParameters = new EncoderParameters(1);
+                EncoderParameter codecParameter = new EncoderParameter(Encoder.Quality, 10L);
+                codecParameters.Param[0] = codecParameter;
+
+                var screenStateLogger = new ScreenStateLogger(codecInfo, codecParameters);
+                screenStateLogger.ScreenRefreshed += (sender, data) =>
+                {
+                    wssv.WebSocketServices.Broadcast(data);
+                };
+                screenStateLogger.Start();
+            }
+            else if (args[0] == "rx")
+            {
+                Console.WriteLine("rx");
+                Console.WriteLine("Enter link:");
+                string link = Console.ReadLine();
+                if (link == null)
+                    return;
+
+                HttpServer server = new HttpServer();
+                server.Start("http://localhost:8000/");
+
+                var ws = new WebSocket(string.Format("ws://{0}/stream", link));
+
+                ws.OnMessage += (sender, message) =>
+                {
+                    server.PutImage(message.RawData);
+                };
+
+                ws.Connect();
+
+
+                System.Diagnostics.Process.Start(new ProcessStartInfo
+                {
+                    FileName = "http://localhost:8000/",
+                    UseShellExecute = true
+                });
+            }
+            else if (args[0] == "txlocal")
+            {
+                Console.WriteLine("txlocal");
+
+                HttpServer server = new HttpServer();
+                server.Start("http://localhost:8000/");
+                
+                //ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+                ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Png);
+                EncoderParameters codecParameters = new EncoderParameters(1);
+                //EncoderParameter codecParameter = new EncoderParameter(Encoder.Quality, 50L);
+                EncoderParameter codecParameter = new EncoderParameter(Encoder.Compression, (long)EncoderValue.CompressionLZW);
+                codecParameters.Param[0] = codecParameter;
+
+                var screenStateLogger = new ScreenStateLogger(codecInfo, codecParameters);
+                screenStateLogger.ScreenRefreshed += (sender, data) =>
+                {
+                    server.PutImage(data);
+                };
+                screenStateLogger.Start();
+
+                server.ParamsChanged += (sender, param) =>
+                {
+                    Params p = (Params)param;
+                    if (p.format == "png")
+                    {
+                        ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Png);
+                        EncoderParameters codecParameters = new EncoderParameters(1);
+                        EncoderParameter codecParameter = new EncoderParameter(Encoder.Compression, (long)EncoderValue.CompressionLZW);
+                        codecParameters.Param[0] = codecParameter;
+                        screenStateLogger.SetImageCodec(codecInfo, codecParameters);
+                    }
+                    else if (p.format == "jpg")
+                    {
+                        ImageCodecInfo codecInfo = GetEncoder(ImageFormat.Jpeg);
+                        EncoderParameters codecParameters = new EncoderParameters(1);
+                        EncoderParameter codecParameter = new EncoderParameter(Encoder.Quality, (long)p.quality);
+                        codecParameters.Param[0] = codecParameter;
+                        screenStateLogger.SetImageCodec(codecInfo, codecParameters);
+                    }
+                };
+
+            }
+            Console.ReadKey(true);
+        }
+
+        static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
+
+
+
